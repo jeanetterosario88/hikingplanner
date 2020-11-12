25bb0a9e8cd3fe19602d6e7981a40d1bd8e496db
jeanetterosario88 <jeanetterosario88@gmail.com>
2020-11-11 23:08:46 -0500

    adding Serializers

--- backend/app/controllers/locations_controller.rb
+++ backend/app/controllers/locations_controller.rb
@@ -1,2 +1,8 @@
 class LocationsController < ApplicationController
+    
+    def show
+        location = Location.find(params[:id])
+        render json: LocationSerializer.new(location)
+    end
+
 end
---
+++ backend/app/serializers/trail_serializer.rb
@@ -0,0 +1,4 @@
+class TrailSerializer
+  include FastJsonapi::ObjectSerializer
+  attributes 
+end
